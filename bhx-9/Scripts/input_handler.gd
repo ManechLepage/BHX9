8f11f975cff7107406033ee644842a9e46505821
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2026-01-16 22:40:51 -0500

    saf

--- bhx-9/Scripts/input_handler.gd
+++ bhx-9/Scripts/input_handler.gd
@@ -9,7 +9,7 @@ var offset: Vector2
 @onready var main: Node2D = $".."
 
 @onready var twitter: WindowUI = %Twitter
-@onready var youtube: WindowUI = %Youtube
+@onready var article: WindowUI = %Article
 @onready var news: WindowUI = %News
 
 enum InputState {
@@ -39,7 +39,7 @@ func unselect_window() -> void:
 
 func focus_selected_window() -> void:
 	twitter.z_index -= 1
-	youtube.z_index -= 1
+	article.z_index -= 1
 	news.z_index -= 1
 	
 	selected_window.z_index = 3
